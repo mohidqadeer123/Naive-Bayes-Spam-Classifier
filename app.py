9dc3a9982e89159d40a013df5cf7ab8e1c474af5
mohidqadeer123 <moqadeer@iu.edu>
2026-01-10 19:13:04 +0000

    Load model and vectorizer

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import streamlit as py
+import pickle
+import numpy as np 
 
+# Load model and vectorizer
+with open("model/model.pkl","rb") as f:
+    model=pickle.load(f)
+
+with open("model/vectorizer.pkl","rb") as f:
+    vectorizer=pickle.load(f)
+
+
+   
