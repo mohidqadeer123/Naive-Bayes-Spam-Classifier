3f591680fcf3cbc806be997dfe97c82853ebb12c
mohidqadeer123 <moqadeer@iu.edu>
2026-01-10 19:29:30 +0000

    Take user input and predict

--- app.py
+++ app.py
@@ -9,5 +9,27 @@ with open("model/model.pkl","rb") as f:
 with open("model/vectorizer.pkl","rb") as f:
     vectorizer=pickle.load(f)
 
+st.set_page_config(page_title="Spam Classifier",layout="centered")
+
+st.title("📩 Spam Detection App")
+st.write("Classifies messages as **Spam** or **Ham** using Multinomial Naive Bayes")
+
+# User input
+user_input=st.text_area("Enter the Message")
+
+if st.button("Predict"):
+    if user_input.strip()==""
+    st.warning("Please enter a message")
+
+    else:
+        transformed = vectorizer.transform([user_input])
+        prediction = model.predict(transformed)[0]
+        probability= model.pred_probab(transformed).max
+
+        if prediction == 1:
+            st.erro(f"🚨 SPAM ({probability:.2%} confidence)")
+
+        else:
+            st.success(f"✅ HAM ({probability:.2%} confidence)")
 
    
